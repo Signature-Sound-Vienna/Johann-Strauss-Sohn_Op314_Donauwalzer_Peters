db3f4fc25b6d9d6e14b04f3f2bb292744666f92a
Werner Goebl <goebl@mdw.ac.at>
2025-02-24 14:51:51 +0100

    Add expand script

---
+++ expand.sh
@@ -0,0 +1,5 @@
+verovio --expand 'Longest_Version_Krauss' -a -t mei -o Longest_Version_Krauss.mei Donauwalzer-Breitkopf.mei
+verovio --expand 'Shorter_Version_Boskovsky' -a -t mei -o Shorter_Version_Boskovsky.mei Donauwalzer-Breitkopf.mei
+verovio --expand 'Short_Version_Karajan_1987' -a -t mei -o Short_Version_Karajan_1987.mei Donauwalzer-Breitkopf.mei
+verovio --expand 'Shortest_Version_Karajan_1946' -a -t mei -o Shortest_Version_Karajan_1946.mei Donauwalzer-Breitkopf.mei
+verovio --expand 'Short_Version_Bauer-Theussl_1983' -a -t mei -o ShortShort_Version_Bauer-Theussl_1983.mei Donauwalzer-Breitkopf.mei
