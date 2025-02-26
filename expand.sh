f8216252a7d7ceb416030b25948e778964fc0548
Werner Goebl <goebl@mdw.ac.at>
2025-02-26 18:04:09 +0100

    Add compress-expanded versions from NYC history

--- expand.sh
+++ expand.sh
@@ -1,5 +1,5 @@
-verovio --expand 'Longest_Version_Krauss' -a -t mei -o Longest_Version_Krauss.mei Donauwalzer-Breitkopf.mei
-verovio --expand 'Shorter_Version_Boskovsky' -a -t mei -o Shorter_Version_Boskovsky.mei Donauwalzer-Breitkopf.mei
-verovio --expand 'Short_Version_Karajan_1987' -a -t mei -o Short_Version_Karajan_1987.mei Donauwalzer-Breitkopf.mei
-verovio --expand 'Shortest_Version_Karajan_1946' -a -t mei -o Shortest_Version_Karajan_1946.mei Donauwalzer-Breitkopf.mei
-verovio --expand 'Short_Version_Bauer-Theussl_1983' -a -t mei -o ShortShort_Version_Bauer-Theussl_1983.mei Donauwalzer-Breitkopf.mei
+verovio --expand 'Longest_Version_Krauss' -a -t mei -o Longest_Version_Krauss.mei IMSLP77157-Donauwalzer-Peters.mei
+verovio --expand 'Shorter_Version_Boskovsky' -a -t mei -o Shorter_Version_Boskovsky.mei IMSLP77157-Donauwalzer-Peters.mei
+verovio --expand 'Short_Version_Karajan_1987' -a -t mei -o Short_Version_Karajan_1987.mei IMSLP77157-Donauwalzer-Peters.mei
+verovio --expand 'Shortest_Version_Karajan_1946' -a -t mei -o Shortest_Version_Karajan_1946.mei IMSLP77157-Donauwalzer-Peters.mei
+verovio --expand 'Short_Version_Bauer-Theussl_1983' -a -t mei -o ShortShort_Version_Bauer-Theussl_1983.mei IMSLP77157-Donauwalzer-Peters.mei
