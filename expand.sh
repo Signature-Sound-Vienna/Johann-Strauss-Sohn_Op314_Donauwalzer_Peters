d5f6d0059ca4bd44cef64d987191e72f285900ad
Werner Goebl <goebl@mdw.ac.at>
2025-03-28 15:50:17 +0100

    Add timemap to expand.sh

--- expand.sh
+++ expand.sh
@@ -1,4 +1,5 @@
 verovio --expand 'Longest_Version_Krauss' -a -t mei -o Longest_Version_Krauss.mei IMSLP77157-Donauwalzer-Peters.mei
+verovio --expand 'Longest_Version_Krauss' -a -t timemap -o Longest_Version_Krauss-timemap.json IMSLP77157-Donauwalzer-Peters.mei
 verovio --expand 'Shorter_Version_Boskovsky' -a -t mei -o Shorter_Version_Boskovsky.mei IMSLP77157-Donauwalzer-Peters.mei
 verovio --expand 'Short_Version_Karajan_1987' -a -t mei -o Short_Version_Karajan_1987.mei IMSLP77157-Donauwalzer-Peters.mei
 verovio --expand 'Shortest_Version_Karajan_1946' -a -t mei -o Shortest_Version_Karajan_1946.mei IMSLP77157-Donauwalzer-Peters.mei
